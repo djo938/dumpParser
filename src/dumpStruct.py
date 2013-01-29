0b357e40bdc22af0673f06c2f456c6a08731c7d7
Jonathan Delvaux <jonathan.delvaux@uclouvain.be>
2013-01-29 18:20:34 +0100

    log parsing done

--- src/dumpStruct.py
+++ src/dumpStruct.py
@@ -2,4 +2,8 @@
 
 class FileDump(object):
     def __init__(self,File):
-        pass #TODO
+        #TODO read the 3 first line in the file
+        #Position : 4519.1764N 00633.7029E, fix time : 093009
+        #Altitude : 2449.300048828125 M, fix time : 093009
+        #Heure : 12h18s9
+        
---
+++ src/dumpStruct.py~
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+class FileDump(object):
+    def __init__(self,File):
+        pass #TODO
